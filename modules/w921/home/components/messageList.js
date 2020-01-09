dc5f398e2c3de465cbe703e21351e19204622135
JERRY <linyonjia12345678@qq.com>
2020-01-09 11:23:41 +0800

    更新

--- modules/w921/home/components/messageList.js
+++ modules/w921/home/components/messageList.js
@@ -7,75 +7,155 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 import Button from '@components/button';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {scaleSizeW} from '../../../../utils/screen';
+import {Steps} from '@ant-design/react-native';
+const Step = Steps.Step;
 
 const styles = StyleSheet.create({
-  button: {},
-  buttonText: {fontSize: setSpText(30), flex: 1},
+  messageList: {
+    marginHorizontal: scaleSizeW(10),
+    marginVertical: scaleSizeH(10),
+    backgroundColor: 'blue',
+  },
+  messageItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: 'red',
+  },
+  messageText: {
+    flex: 1,
+    fontSize: setSpText(30),
+  },
+  tagItem: {
+    height: scaleSizeH(50),
+    width: scaleSizeW(30),
+    borderRadius: setSpText(10),
+    justifyContent: 'center',
+  },
+  tagText: {
+    fontWeight: 'bold',
+    fontSize: setSpText(25),
+    textAlign: 'center',
+  },
 });
 
+/* 单条消息*/
 function Message(props) {
   return (
-    <TouchableWithoutFeedback>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-          marginLeft: 20,
-          borderLeftWidth: 1,
-          borderColor: '#00BFFF',
-          position: 'relative',
-          paddingTop: 20,
-        }}>
-        <View
-          style={{
-            width: scaleSizeW(7),
-            height: scaleSizeH(20),
-            backgroundColor: 'red',
-            borderRadius: 20,
-            position: 'absolute',
-            left: -7,
-          }}
+    <View style={{backgroundColor: 'yellow'}}>
+      <Steps
+        style={{width: '100%', backgroundColor: 'red'}}
+        size="small"
+        current={-1}
+        direction="vertical">
+        <Step
+          key={props.id}
+          title={
+            <TouchableWithoutFeedback
+              onPress={() =>
+                props.onPress('message', {
+                  serviceId: props.serviceId,
+                  callerSN: props.callerSN,
+                  tagName: props.tagName,
+                })
+              }>
+              <View style={styles.messageItem}>
+                <Text style={styles.messageText}>{props.name}</Text>
+                <View style={[styles.tagItem, {backgroundColor: props.color}]}>
+                  <Text style={styles.tagText}>{props.callType} </Text>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          }
+          description={<Text>{props.time}</Text>}
         />
-        <View style={{paddingLeft: 10}}>
-          <Text
-            style={{
-              fontSize: setSpText(30),
-              color: Color('rgb(74,125,200)').lighten(0.2),
-            }}>
-            {this.props.name}
-          </Text>
-          <Text style={{color: Color('rgb(74,125,200)').lighten(0.5)}}>
-            {this.props.time}
-          </Text>
-        </View>
-        <View
-          style={{
-            height: scaleSizeH(50),
-            width: scaleSizeW(30),
-            backgroundColor: props.color,
-            borderRadius: 10,
-            justifyContent: 'center',
-          }}>
-          <Text style={styles.tag}>{props.callType} </Text>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </Steps>
+    </View>
   );
 }
 
+const data = [
+  {
+    id: 1,
+    name: '中餐厅大厅1号桌',
+    callType: '服务',
+    color: '#32CD32',
+    time: '5分钟前',
+    serviceId: 1,
+    callerSN: 'sn1',
+    tagName: '呼叫',
+  },
+  {
+    id: 2,
+    name: '中餐厅大厅2号桌',
+    callType: '点菜',
+    color: 'orange',
+    time: '7分钟前',
+    serviceId: 1,
+    callerSN: 'sn1',
+    tagName: '结账',
+  },
+  {
+    id: 3,
+    name: '中餐厅大厅3号桌',
+    callType: '点菜',
+    color: 'orange',
+    time: '2分钟前',
+    serviceId: 2,
+    callerSN: 'sn2',
+    tagName: '呼叫',
+  },
+  {
+    id: 4,
+    name: '中餐厅大厅4号桌',
+    callType: '结账',
+    color: 'red',
+    time: '几秒前',
+    serviceId: 2,
+    callerSN: 'sn2',
+    tagName: '结账',
+  },
+  {
+    id: 5,
+    name: '中餐厅大厅5号桌',
+    callType: '结账',
+    color: 'red',
+    time: '3分钟前',
+    serviceId: 1,
+    callerSN: 'sn1',
+    tagName: '呼叫',
+  },
+  {
+    id: 6,
+    name: '中餐厅大厅6号桌',
+    callType: '服务',
+    color: '#32CD32',
+    time: '1分钟前',
+    serviceId: 1,
+    callerSN: 'sn1',
+    tagName: '呼叫',
+  },
+  {
+    id: 7,
+    name: '中餐厅大厅7号桌',
+    callType: '服务',
+    color: '#32CD32',
+    time: '几秒前',
+    serviceId: 2,
+    callerSN: 'sn2',
+    tagName: '呼叫',
+  },
+];
 export default function Operations(props) {
   return (
     <View>
-      {/* <FlatList
-        data={this.props.data}
-        keyExtractor={(item, index) => item.id + ''}
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
         showsVerticalScrollIndicator={false}
         windowSize={200}
         onEndReachedThreshold="0.4"
-        style={{borderWidth: 1, borderColor: '#00BFFF', paddingTop: 30}}
+        style={styles.messageList}
         // onEndReached={() => this.loadMore()}
         ListFooterComponent={<View style={{height: scaleSizeH(60)}}></View>}
         renderItem={({item}) => (
@@ -84,16 +164,11 @@ export default function Operations(props) {
             callType={item.callType}
             color={item.color}
             time={item.time}
-            openDrawer={type =>
-              this.props.openDrawer(type, {
-                serviceId: item.serviceId,
-                callerSN: item.callerSN,
-                tagName: item.tagName,
-              })
-            }
+            openDrawer={props.openDrawer}
           />
         )}
-      /> */}
+      />
+
       <Button>gahga</Button>
       <Button>gahga</Button>
       <Button>gahga</Button>
